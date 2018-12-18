bdeef50de204a17ffcf3159e4a7b3ffbd7490ebc
CAJan93 <jan.mensch@gmx.net>
2018-12-18 19:39:01 +0100

    clean up

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -13,46 +13,6 @@ object Sindy {
 
       import spark.implicits._
 
-      /*
-      val region = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv(inputs(0)) // also text, json, jdbc, parquet
-        // .as[String]// String)]
-
-      try to infer type
-      val nation = spark.read
-          .option("inferSchema", "true")
-          .option("header", "true")
-          .csv(inputs(1))
-          // .as[(Int, String, Int, String)]
-      */
-
-/*
-      val region = spark
-        .read
-        .option("inferSchema", "false")
-        .option("header", "true")
-        .option("quote", "\"")
-        .option("delimiter", ";")
-        .csv(inputs(0))
-        .toDF("region_key", "region_name", "comment1")
-        .as[(String, String, String)] // do not use Int, because you might get cast err
-
-       val nation = spark
-         .read
-         .option("inferSchema", "false")
-         .option("header", "true")
-         .option("quote", "\"")
-         .option("delimiter", ";")
-         .csv(inputs(1))
-         .toDF("nation_key", "nation_name", "region_key", "comment2")
-         .as[(String, String, String, String)]
-
-      nation.show(10)
-      region.show(10)
-      */
-
 
 
     val region = spark
@@ -110,93 +70,9 @@ object Sindy {
 
     tmp.foreach(println)
 
-    /*
-      val tmp : RDD[(Set[String])] = result
-          .map(k_v => (k_v._2, k_v._1))
-          .reduceByKey(_ ++ _)
-          .map(k_v => k_v._1)
-
-      tmp.foreach(println)
-      */
-
-        //.fold(Set[String]())((acc, row) => {
-    //  acc :+ row._2
-    //   })
-
-
-    // result.foreach(println)
-
-
-    println("----------------------------------------------------------------------------------------")
-
-    val result1 : RDD[(String, Set[String])] = result0
-      .union(result)
-        .reduceByKey(_ ++ _)
-
-   // result1.foreach(println)
 
 
 
-
-/*
-
-    val result2 : RDD[(String, Set[String])] = nation2
-      .flatMap(row => row.toString().filterNot("[]".toSet).split(",").view.zipWithIndex.foldLeft(Seq[(String, Set[String])]())((a, b) => {
-        println(b._2)
-        a :+ (b._1.toString(), Set(scema(b._2)))
-      }))
-      .rdd
-      .reduceByKey(_ ++ _)
-
-//    result2.foreach(println)
-
-
-    println("\n---------------------------------------------------------------------------------------------------\n")
-
-    val result  : RDD[(String, Set[String])] = nation
-      .flatMap(row => Seq((row._1, Set(scema(0))), (row._2, Set(scema(1))), (row._3, Set(scema(2))), (row._4, Set(scema(3)))))
-      .rdd
-      .reduceByKey(_ ++ _)  // RDD[(String, Set[String])]
-
-    // result.foreach(println)
-
-*/
-    /*
-      // works, but hard coded
-      val result  : RDD[(String, Set[String])] = nation
-          .flatMap(row =>  Seq((row._1, Set("nation_key")), (row._2, Set("nation_name")), (row._3, Set("region_key")), (row._4, Set("comment2"))))
-          .rdd
-          .reduceByKey(_ ++ _)  // RDD[(String, Set[String])]
-*/
-
-
-    // result.foreach(println(_))
-
-    // val tmp : Int  =  result // check type
-
-
-
-
-
-        /*
-          .map(row => {
-            // each ele in row with head of row
-            ((row(0), "region_key"), (row(1), "region_name"), (row(2), "comment1"),
-            (row(3), "nation_key"), (row(4), "nation_name"), (row(5), "region_key"),
-            (row(6), "comment2"))
-          })
-          .reduce(row => 1)
-*/
-          //.join(region,
-            //usingColumns = Seq("region_key", "region_name", "comment1", "nation_key", "nation_name", "comment2"),  "full_outer")
-          // .repartition(32)
-          // .flatMap(line => line  .split(";"))
-          // .map(line => line)
-    // .flatMap((key:String, name:String, reg:String, cmd:String) => List(key, name, reg, cmd))
-        // .map((key, name:String, reg:String, cmd:String):String  => "") // convert to dataset
-        // .flatMap(line => line)
-
-//      println(result.getClass())
 
   }
 }
